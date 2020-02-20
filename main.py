196798fb206eda1625aa1f4a831687bc5a8df988
aaronalt <aaronalt07@gmail.com>
2020-02-20 15:03:10 +0300

    format changes

--- main.py
+++ main.py
@@ -36,6 +36,37 @@ class PERT:
             print("Invalid input, press [enter] or 'n'")
             self.brain()
 
+    def hours_to_weeks(self):
+        week_o = 0
+        week_p = 0
+        week_r = 0
+        week_mean = 0
+        std_dev = 0
+        self.weeks = [[x / 8 for x in i] for i in self.lol]
+        for i in self.weeks:
+            mean = (i[0] + 4 * i[1] + i[2]) / 6
+            std_dev = (i[2] - i[0]) / 6
+            i.append(round(mean, 2))
+            i.append(round(std_dev, 4))
+        print("\n============================\n")
+        print("Duration Estimate (in weeks)")
+        print("\n-------\n")
+        print("data:")
+        for i in self.weeks:
+            print("\t", i)
+            week_o += i[0]
+            week_r += i[1]
+            week_p += i[2]
+            week_mean += i[3]
+            std_dev += i[4]
+        print("\n-------\n")
+        print(f"Mean: {week_mean}")
+        print(f"Standard deviation: {round(std_dev)}")
+        print(f"Total optimistic: {week_o}")
+        print(f"Total realistic: {week_r}")
+        print(f"Total pessimistic: {week_p}")
+        print("\n==============\n")
+
     def calculation(self):
         for i in self.lol:
             self.o = i[0]
@@ -51,45 +82,19 @@ class PERT:
             self.total_o += i[0]
             self.total_r += i[1]
             self.total_p += i[2]
-        print("List of data:")
+        print("Cost Estimate (in units)")
+        print("\n-------\n")
+        print("data:")
         for i in self.lol:
             print("\t", i)
-        print("\n============================\n")
-        print(f"Total PERT estimate: {round(self.total_mean, 2)}")
+        print("\n-------\n")
+        print(f"Total PERT estimate (mean): {round(self.total_mean, 2)}")
         print(f"Total deviation: {self.total_std_dev}")
         print(f"Total optimistic: {self.total_o}")
         print(f"Total realistic: {self.total_r}")
         print(f"Total pessimistic: {self.total_p}")
         print("\n============================\n")
         print("we are done here...bye")
-
-    def hours_to_weeks(self):
-        week_o = 0
-        week_p = 0
-        week_r = 0
-        week_mean = 0
-        std_dev = 0
-        self.weeks = [[x / 8 for x in i] for i in self.lol]
-        for i in self.weeks:
-            mean = (i[0] + 4 * i[1] + i[2]) / 6
-            std_dev = (i[2] - i[0]) / 6
-            i.append(round(mean, 2))
-            i.append(round(std_dev, 4))
-        print(".\n...\n.....\n.......")
-        print("Weeks data:")
-        for i in self.weeks:
-            print("\t", i)
-            week_o += i[0]
-            week_r += i[1]
-            week_p += i[2]
-            week_mean += i[3]
-            std_dev += i[4]
-        print(f"Mean week estimate: \n\t{week_mean}")
-        print(f"Standard deviation: \n\t{std_dev}")
-        print(f"Optimistic week estimate: \n\t{week_o}")
-        print(f"Realistic week estimate: \n\t{week_r}")
-        print(f"Pessimistic week estimate: \n\t{week_p}")
-        # print(".......\n.....\n...\n.")
 
 
 def main():
